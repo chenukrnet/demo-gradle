15198ba15bf984cb36785768bfaca81240a13849
Smoljanov Sergej <chen@ukr.net>
2020-11-08 11:41:52 +0200

    init commit

---
+++ src/main/java/com/home/gradle/gradleprojectstructure/GradleProjectStructureApplication.java
@@ -0,0 +1,19 @@
+package com.home.gradle.gradleprojectstructure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Slf4j
+@SpringBootApplication
+public class GradleProjectStructureApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleProjectStructureApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("started");
+    }
+}
---
+++ src/test/java/com/home/gradle/gradleprojectstructure/GradleProjectStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.gradle.gradleprojectstructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleProjectStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
