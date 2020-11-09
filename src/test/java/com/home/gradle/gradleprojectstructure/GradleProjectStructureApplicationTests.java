4697216044bb68a8a5c10f2315240c511865937a
Smoljanov Sergej <chen@ukr.net>
2020-11-09 19:17:52 +0200

    test with lombok annotation

--- src/test/java/com/home/gradle/gradleprojectstructure/GradleProjectStructureApplicationTests.java
+++ src/test/java/com/home/gradle/gradleprojectstructure/GradleProjectStructureApplicationTests.java
@@ -1,8 +1,9 @@
 package com.home.gradle.gradleprojectstructure;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Slf4j
 @SpringBootTest
 class GradleProjectStructureApplicationTests {
 
