863f969a90946d3c1600179863fe1e861a95b2bd
Smoljanov Sergej <chen@ukr.net>
2020-11-25 20:09:32 +0200

    дополнил билд коментариями и дополнил тест

--- src/test/java/com/home/gradle/gradleprojectstructure/GradleProjectStructureApplicationTests.java
+++ src/test/java/com/home/gradle/gradleprojectstructure/GradleProjectStructureApplicationTests.java
@@ -1,8 +1,13 @@
 package com.home.gradle.gradleprojectstructure;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 @Slf4j
 @SpringBootTest
 class GradleProjectStructureApplicationTests {
@@ -11,4 +16,15 @@ class GradleProjectStructureApplicationTests {
     void contextLoads() {
     }
 
+    @Tag("integration")
+    @Test
+    void integrationTest(){
+        fail();
+    }
+
+    @Disabled
+    @Test
+    void disabled(){
+        fail();
+    }
 }
